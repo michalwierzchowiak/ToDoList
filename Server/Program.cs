2f5b21883a1f40746f8910f3e032a1e96e4e0527
maiky999 <mich.wierzchowiak@gmail.com>
2026-01-18 15:23:27 +0100

    Implementacja serwera back-end

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,23 +1,31 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using Server;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSignalR(); // SignalR
+builder.Services.AddDbContext<DatabaseAppContext>(opt => opt.UseInMemoryDatabase("TodoList"));
+
+builder.Services.AddCors(policy =>
+{
+    policy.AddPolicy("AllowAll", builder =>
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader());
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
-app.UseAuthorization();
+app.UseCors("AllowAll");
 
 app.MapControllers();
+app.MapHub<TodoHub>("/todohub");
 
 app.Run();
